96f93c4e4b7ef4e91bd2e945fe4fb30d507c8cf3
rsharifu <clarus09@gmail.com>
2016-01-24 21:55:10 -0800

    Update controller with RSS feed parser function Implement List view. Add side menu.

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,11 @@
 // Ionic Starter App
-
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
 
@@ -20,58 +19,32 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 // On module loading
 .config(function($stateProvider, $urlRouterProvider) {
-
   $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
+    .state('app', {
+      url: '/app',
+      abstract: true,
+      templateUrl: 'templates/menu.html',
+      controller: 'MainCtrl'
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
+    .state('app.settings', {
+        url: '/settings',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/settings.html'
+          }
+        }
+    })
+    .state('app.feed-list', {
+      url: '/feed-list',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        'menuContent': {
+          templateUrl: 'templates/feed-list.html',
+          controller: 'MainCtrl'
         }
       }
     })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+    $urlRouterProvider.otherwise('/app/feed-list');
+})
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
 
-});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,13 +2,6 @@ angular.module('starter.controllers', [])
 
 .controller('MainCtrl', function($http, $scope) {
 
-$scope.data = {
-    showDelete: false
-  };
-
-  $scope.edit = function(item) {
-    alert('Edit Item: ' + item.id);
-  };
   $scope.share = function(item) {
     alert('Share Item: ' + item.id);
   };
@@ -18,63 +11,6 @@ $scope.data = {
     $scope.items.splice(toIndex, 0, item);
   };
 
-  $scope.onItemDelete = function(item) {
-    $scope.items.splice($scope.items.indexOf(item), 1);
-  };
-
-  $scope.items = [
-    { id: 0 },
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
-    { id: 6 },
-    { id: 7 },
-    { id: 8 },
-    { id: 9 },
-    { id: 10 },
-    { id: 11 },
-    { id: 12 },
-    { id: 13 },
-    { id: 14 },
-    { id: 15 },
-    { id: 16 },
-    { id: 17 },
-    { id: 18 },
-    { id: 19 },
-    { id: 20 },
-    { id: 21 },
-    { id: 22 },
-    { id: 23 },
-    { id: 24 },
-    { id: 25 },
-    { id: 26 },
-    { id: 27 },
-    { id: 28 },
-    { id: 29 },
-    { id: 30 },
-    { id: 31 },
-    { id: 32 },
-    { id: 33 },
-    { id: 34 },
-    { id: 35 },
-    { id: 36 },
-    { id: 37 },
-    { id: 38 },
-    { id: 39 },
-    { id: 40 },
-    { id: 41 },
-    { id: 42 },
-    { id: 43 },
-    { id: 44 },
-    { id: 45 },
-    { id: 46 },
-    { id: 47 },
-    { id: 48 },
-    { id: 49 },
-    { id: 50 }
-  ];
 
 $scope.init = function() {
         $http.get("/sample.xml/sample.xml")
@@ -82,13 +18,13 @@ $scope.init = function() {
                 var x2js = new X2JS();
                 var jsonData = x2js.xml_str2json(data);
                 console.log("jsonData=" + jsonData.rss.channel.title);
-//                console.log("data=" + data);
-                console.log("data.responseData=" + data.responseData);
-                $scope.rssTitle = data.responseData.feed.title;
-                $scope.rssUrl = data.responseData.feed.feedUrl;
-                $scope.rssSiteUrl = data.responseData.feed.link;
-                $scope.entries = data.responseData.feed.entries;
-                window.localStorage["entries"] = JSON.stringify(data.responseData.feed.entries);
+                console.log("jsonData.rss.channel.items=" + jsonData.rss.channel.item);
+//                console.log("data.responseData=" + data.responseData);
+//                $scope.rssTitle = data.responseData.feed.title;
+//                $scope.rssUrl = data.responseData.feed.feedUrl;
+//                $scope.rssSiteUrl = data.responseData.feed.link;
+                $scope.entries = jsonData.rss.channel.item;
+//                window.localStorage["entries"] = JSON.stringify(jsonData.rss.channel.item);
             })
             .error(function(data) {
                 console.log("ERROR: " + data);
